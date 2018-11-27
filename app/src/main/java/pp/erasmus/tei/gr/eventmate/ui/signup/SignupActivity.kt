c08ad25e8ad8f6808a220be8a49e558cfc54ca54
Petra Cendelínová <cendelinova@master.cz>
2018-11-27 23:40:02 +0200

    some login layout

---
+++ app/src/main/java/pp/erasmus/tei/gr/eventmate/ui/signup/SignupActivity.kt
@@ -0,0 +1,13 @@
+package pp.erasmus.tei.gr.eventmate.ui.signup
+
+import android.os.Bundle
+import pp.erasmus.tei.gr.eventmate.R
+import pp.erasmus.tei.gr.eventmate.ui.base.BaseActivity
+
+class SignupActivity : BaseActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_signup)
+	}
+}
