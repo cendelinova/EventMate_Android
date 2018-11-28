2afb61b219ffec0c483c84d8ffe222937a1b2d5d
Petra Cendelínová <cendelinova@master.cz>
2018-11-28 20:20:19 +0200

    Login screen

--- app/src/main/java/pp/erasmus/tei/gr/eventmate/ui/base/BaseActivity.kt
+++ app/src/main/java/pp/erasmus/tei/gr/eventmate/ui/base/BaseActivity.kt
@@ -1,5 +1,13 @@
 package pp.erasmus.tei.gr.eventmate.ui.base
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
-abstract class BaseActivity: AppCompatActivity() {}
+abstract class BaseActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		
+		window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+	}
+}
