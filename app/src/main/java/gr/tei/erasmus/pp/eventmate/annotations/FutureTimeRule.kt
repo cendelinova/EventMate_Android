d1de457d64ce7c0216f88e54f415fcef905cc349
Petra Cendelínová <cendelinova@master.cz>
2018-12-05 13:11:38 +0200

    More validation on event form

--- app/src/main/java/gr/tei/erasmus/pp/eventmate/annotations/FutureTimeRule.kt
+++ app/src/main/java/gr/tei/erasmus/pp/eventmate/annotations/FutureTimeRule.kt
@@ -2,18 +2,16 @@ package gr.tei.erasmus.pp.eventmate.annotations
 
 import com.mobsandgeeks.saripaar.AnnotationRule
 import gr.tei.erasmus.pp.eventmate.helpers.DateTimeHelper
-import timber.log.Timber
-import java.text.ParseException
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import timber.log.Timber
+import java.text.ParseException
 
-
-
-class FutureDateRule(ruleAnnotation: FutureDate?) : AnnotationRule<FutureDate, String>(ruleAnnotation) {
+class FutureTimeRule(ruleAnnotation: FutureTime?) : AnnotationRule<FutureTime, String>(ruleAnnotation) {
 	override fun isValid(dateString: String?): Boolean {
-		val formatter = DateTimeFormat.forPattern(DateTimeHelper.DATE_FORMAT)
+		val formatter = DateTimeFormat.forPattern(DateTimeHelper.DATE_TIME_FORMAT)
 		var parsedDate: DateTime? = null
-
+		
 		if (dateString.isNullOrEmpty()) return false
 		
 		try {
@@ -22,6 +20,6 @@ class FutureDateRule(ruleAnnotation: FutureDate?) : AnnotationRule<FutureDate, S
 			Timber.e(ignored)
 		}
 		
-		return parsedDate != null && (parsedDate.isAfterNow || DateTimeHelper.isToday(parsedDate))
+		return parsedDate != null && parsedDate.isAfterNow
 	}
 }
--- app/src/main/java/gr/tei/erasmus/pp/eventmate/helpers/DateTimeHelper.kt
+++ app/src/main/java/gr/tei/erasmus/pp/eventmate/helpers/DateTimeHelper.kt
@@ -1,8 +1,14 @@
 package gr.tei.erasmus.pp.eventmate.helpers
 
+import org.joda.time.DateTime
+import org.joda.time.LocalDate
+
 class DateTimeHelper {
 	companion object {
 		const val DATE_FORMAT = "MM/dd/yyyy"
 		const val TIME_FORMAT = "HH:mm"
+		const val DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+		
+		fun isToday(dateTime: DateTime): Boolean = (dateTime.toLocalDate()) == (LocalDate())
 	}
 }
