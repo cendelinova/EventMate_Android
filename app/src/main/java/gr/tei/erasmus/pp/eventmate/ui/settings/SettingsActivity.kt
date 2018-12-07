322bc37619acacd1c7bebd2131ee5d5f78c4661c
Petra Cendelínová <cendelinova@master.cz>
2018-12-07 16:29:45 +0200

    Fixed margin among input on new event form

--- app/src/main/java/gr/tei/erasmus/pp/eventmate/ui/settings/SettingsActivity.kt
+++ app/src/main/java/gr/tei/erasmus/pp/eventmate/ui/settings/SettingsActivity.kt
@@ -1,15 +1,14 @@
 package gr.tei.erasmus.pp.eventmate.ui.settings
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.preference.*
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import gr.tei.erasmus.pp.eventmate.R
-import gr.tei.erasmus.pp.eventmate.app.App
 import gr.tei.erasmus.pp.eventmate.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_settings.*
+
 
 class SettingsActivity : BaseActivity() {
 	
@@ -22,29 +21,11 @@ class SettingsActivity : BaseActivity() {
 		supportFragmentManager.beginTransaction().replace(R.id.settings_root, EventMatePreferenceFragment()).commit()
 	}
 	
-	class EventMatePreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
-		private var sharedPreferenceHelper = App.COMPONENTS.provideSharedPreferencesHelper()
-		
-		override fun onResume() {
-			super.onResume()
-			preferenceScreen.sharedPreferences
-				.registerOnSharedPreferenceChangeListener(this)
-		}
-		
-		override fun onPause() {
-			super.onPause()
-			preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
-		}
-		
+	class EventMatePreferenceFragment : PreferenceFragmentCompat() {
 		override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 			setPreferencesFromResource(R.xml.preferences, rootKey)
 		}
 		
-		override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-			val preference = findPreference(key)
-			if (preference is SwitchPreferenceCompat) preference.isChecked =
-					sharedPreferenceHelper.loadBoolean(preference.key)
-		}
 		
 		override fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*> {
 			return object : PreferenceGroupAdapter(preferenceScreen) {
@@ -52,13 +33,9 @@ class SettingsActivity : BaseActivity() {
 					super.onBindViewHolder(holder, position)
 					val preference = getItem(position)
 					
-					if (preference is PreferenceCategory)
-						setZeroPaddingToLayoutChildren(holder.itemView)
-					else {
-						holder.itemView.findViewById<View?>(R.id.icon_frame)?.visibility =
-								if (preference.icon == null) View.GONE else View.VISIBLE
-						
-					}
+					holder.itemView.findViewById<View?>(R.id.icon_frame)?.visibility = View.GONE
+					
+					if (preference is PreferenceCategory) setZeroPaddingToLayoutChildren(holder.itemView)
 				}
 			}
 		}
@@ -75,17 +52,6 @@ class SettingsActivity : BaseActivity() {
 			}
 		}
 		
-		override fun onPreferenceTreeClick(preference: Preference?): Boolean {
-			preference?.run {
-				if (preference is SwitchPreferenceCompat) sharedPreferenceHelper.saveBoolean(
-					preference.key,
-					preference.isChecked
-				)
-				
-			}
-			
-			return super.onPreferenceTreeClick(preference)
-		}
 	}
 	
 }
