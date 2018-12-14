cfb501e659044b93e5be2eaf499e649d04712d31
Petra Cendelínová <cendelinova@master.cz>
2018-12-14 12:56:48 +0200

    Init task detail layout

--- app/src/main/java/gr/tei/erasmus/pp/eventmate/ui/taskDetail/TaskDetailActivity.kt
+++ app/src/main/java/gr/tei/erasmus/pp/eventmate/ui/taskDetail/TaskDetailActivity.kt
@@ -3,11 +3,16 @@ package gr.tei.erasmus.pp.eventmate.ui.taskDetail
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import gr.tei.erasmus.pp.eventmate.R
+import gr.tei.erasmus.pp.eventmate.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.toolbar_task_detail.*
 
-class TaskDetailActivity : AppCompatActivity() {
+class TaskDetailActivity : BaseActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_task_detail)
+		
+		setupToolbar(toolbar)
+		
 	}
 }
